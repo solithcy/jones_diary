1a94754b6409cfe58516d5fc96397fe01c220ee7
Salma Eltayeb <eltayebsalma@outlook.com>
2025-07-25 11:12:14 +0100

    adding routes

--- server/router/entryRoute.js
+++ server/router/entryRoute.js
@@ -3,9 +3,12 @@ const { Router } = require ('express')
 const entryController = require("../controller/entryController");
 const entryRouter = Router();
 
-entryRouter.get("/", entryController.index);
+entryRouter.get("/category/:name", entryController.index);
 entryRouter.get("entries/:id", entryController.show);
+entryRouter.get("/date/:date", entryController.date)
+entryRouter.get("/me", entryController.profile)
 entryRouter.post("/login", userController.login);
 entryRouter.post("/register", userController.register)
 entryRouter.post("/entries", entryController.create)
-entryController.patch("/entries/:id", )
+entryRouter.patch("/entries/:id", entryController.authenticate)
+entryRouter.delete("/enteries/:id", entryController.destroy)

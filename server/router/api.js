6fe843da79161c1820254f4f20c6cdea6f946e38
solithcy <me@rose.gay>
2025-07-25 12:57:44 +0100

    add auth middleware

--- server/router/api.js
+++ server/router/api.js
@@ -2,12 +2,13 @@ const { Router } = require ('express')
 
 const entryController = require("../controller/entryController");
 const userController = require("../controller/userController");
+const auth = require("../middleware/auth");
 const router = Router();
 
 // router.get("/category/:name", entryController.index);
 // router.get("/entries/:id", entryController.show);
 // router.get("/date/:date", entryController.date);
-router.get("/me", userController.me)
+router.get("/me", auth, userController.me)
 router.post("/login", userController.login);
 router.post("/register", userController.register);
 // router.post("/entries", entryController.create);

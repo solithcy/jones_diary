d6ae7ee9c7a2dcce8160659bc3b5dd00bc2a128e
solithcy <me@rose.gay>
2025-07-25 13:34:36 +0100

    rename variable, clean up typos

--- server/model/entryModel.js
+++ server/model/entryModel.js
@@ -2,17 +2,17 @@ const db = require('../database/connect.js')
 
 class Entry {
 
-    constructor({id, uid, content, category, timestamp}) {
+    constructor({id, uid, content, category, created}) {
         this.id = id
         this.uid = uid
         this.content = content
         this.category = category
-        this.timestamp = timestamp 
+        this.created = created
     }
 
     static async getEntryByCategory(cat) {
         const result = await db.query("SELECT * FROM entries WHERE category = $1", [cat])
-        return result.rows.map(u = new Entry(u))
+        return result.rows.map(u => new Entry(u))
     }
 
     static async getAllByUid(uid) {
@@ -31,18 +31,18 @@ class Entry {
     }
 
     static async createEntry({content, category, date}) {
-        const result = await db.query("INSERT INTO enteries (content, category, date) VALUES ($1, $2, $3) RETURINING *", [content, category, date])
+        const result = await db.query("INSERT INTO entries (content, category, date) VALUES ($1, $2, $3) RETURNING *", [content, category, date])
         return result.rows[0];
     }
 
     async updateEntry({content, category}) {
         const result = await db.query(
-            "UPDATE enteries SET content = $1, category = $2 WHERE id = $3 RETURNING *")
+            "UPDATE entries SET content = $1, category = $2 WHERE id = $3 RETURNING *")
         return result.rows[0]
     }
 
     async deleteEntry(id){
-        const result = await db.query("DELETE FROM enteries WHERE id = $1 RETURNING *", [id])
+        const result = await db.query("DELETE FROM entries WHERE id = $1 RETURNING *", [id])
         return result.rows[0]
     }
 }
